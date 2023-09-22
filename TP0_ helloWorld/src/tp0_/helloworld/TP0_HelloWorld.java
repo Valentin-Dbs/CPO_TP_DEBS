50c8bbbcb5071f06b73342ddea2b2659a861a6f7
Valentin Debs <iut@172.24.19.51>
2023-09-22 11:55:24 +0200

    TP0_HelloWorld

---
+++ TP0_ helloWorld/src/tp0_/helloworld/TP0_HelloWorld.java
@@ -0,0 +1,27 @@
+/*
+ * TP0 Exercice 1
+ * MRG
+ * Septembre 2023
+ */
+package tp0_.helloworld;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author iut
+ */
+public class TP0_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String prenom;
+        Scanner sc;
+        sc = new Scanner (System.in);
+        System.out.println("Quel est votre prénom ?");
+        prenom = sc.nextLine();
+    }
+    
+}
