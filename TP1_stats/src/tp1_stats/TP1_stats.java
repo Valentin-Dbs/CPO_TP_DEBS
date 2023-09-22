a590f69f0d81bf6282436222894c0a8b81a5f706
Valentin Debs <iut@172.24.19.51>
2023-09-22 14:50:27 +0200

    TP1_Stats (Pas fini)

---
+++ TP1_stats/src/tp1_stats/TP1_stats.java
@@ -0,0 +1,46 @@
+/*
+ * TP1_stats
+ * Vérifier si la distribution de nombre du générateur de nombre aléatoire est bien aléatoire
+ * Septembre 2023
+ */
+package tp1_stats;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author iut
+ */
+public class TP1_stats {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //initialisation du tableau :
+        int[] tableau = new int[6];
+
+        //Initialisation du scanner :
+        Scanner sc = new Scanner(System.in);
+
+        //Création du générateur aléatoire
+        Random nombreAleatoire = new Random();
+        int nombre;
+
+        //Récupération du nombre
+        System.out.println("Saisissez un nombre :");
+        int m = sc.nextInt();
+
+        //Boucle
+        while (m > 0) {
+            m -= 1;
+            nombre = nombreAleatoire.nextInt(5);
+            tableau[nombre] += 1;
+        }
+        for(int i=0; i<6;i++){
+            System.out.println(tableau[i]);
+        }
+    }
+
+}
