97d6a2f2b8bde62900bae0d35c2b26241364275f
Valentin Debs <iut@172.24.19.51>
2023-10-20 17:59:52 +0200

    Super Puissance 4 : des trous noirs et des désintégrateurs à la partie graphique

--- SuperPuissance4_console/src/superpuissance4_console/fenetreDeJeu.java
+++ SuperPuissance4_console/src/superpuissance4_console/fenetreDeJeu.java
@@ -210,6 +210,8 @@ public class fenetreDeJeu extends javax.swing.JFrame {
         panneau_info_joueurs.setVisible(true);
         panneau_info_partie.setVisible(true);
         initialiserPartie();
+        panneau_grille.repaint();
+        btn_start.setEnabled(false);
     }//GEN-LAST:event_btn_startActionPerformed
 
     /**
